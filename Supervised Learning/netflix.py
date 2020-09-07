1e2ce3663694af7c72ff66191da624e129e0ec4e
zachfloro <zachfloro13@gmail.com>
2020-09-07 13:56:03 -0400

    added sklearn attribution to netflix.py

--- Supervised Learning/netflix.py
+++ Supervised Learning/netflix.py
@@ -1,7 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep  7 13:20:57 2020
-
-@author: Zach
-"""
-
+'''
+ATTRIBUTIONS:
+    This script makes use of numerous open source software packages that provide machine learning algorithm implementations, plotting tools, etc.
+    For the decision tree, boosting, support vector machine and K-NN algorithm implementations I used the package Sklearn.  Please see the following paper for more specific information:
+        Scikit-learn: Machine Learning in Python, Pedregosa et al., JMLR 12, pp. 2825-2830, 2011. 
+        (https://scikit-learn.org/)
+'''
