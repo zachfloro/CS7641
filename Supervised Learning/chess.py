9e6153c83f11cf100feac93ea33b0423713f741b
zachfloro <zachfloro13@gmail.com>
2020-09-20 14:26:47 -0400

    removed math import from chess.py

--- Supervised Learning/chess.py
+++ Supervised Learning/chess.py
@@ -16,7 +16,6 @@ ATTRIBUTIONS:
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import math
 import torch
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
